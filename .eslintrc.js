77cacb47ebe08ff86d278bb99efaac91bc29ef55
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-08-04 07:43:19 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: "airbnb-base",
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                ".eslintrc.{js,cjs}",
+            ],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double", { allowTemplateLiterals: true }],
+        indent: ["error", 4],
+        "eol-last": ["error", "never"],
+        "linebreak-style": 0,
+    },
+};
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "../style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <p class = "bg-red-900 text-white">Hello</p>
+
+  </div>
+`;
